947e1e216f300f85d05ae8cda5b24ee0a77dc300
Johyn Papin <contact@johyn.me>
2020-09-11 22:22:38 +0200

    Removed Firebase files

--- panel/src/plugins/firebase.js
+++ panel/src/plugins/firebase.js
@@ -1,16 +1,7 @@
 import * as firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyCIXiusoOXXsbzFHULlEF4ADrTIvV24DLY',
-  authDomain: 'rostrenen-et-moi.firebaseapp.com',
-  databaseURL: 'https://rostrenen-et-moi.firebaseio.com',
-  projectId: 'rostrenen-et-moi',
-  storageBucket: 'rostrenen-et-moi.appspot.com',
-  messagingSenderId: '398521852046',
-  appId: '1:398521852046:web:1f5a10044783c9c2ffa9df'
-}
+import firebaseConfig from './firebase-config'
 
 firebase.initializeApp(firebaseConfig)
 
